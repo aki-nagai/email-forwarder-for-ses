4268d03481bae91421dbfdfb6e6a43c218904926
aki-nagai <akihito.nagai@acrorea.com>
2017-02-28 12:34:55 +0900

    retrieve ses notification and output log to cloudwatch

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // Func: Lambda handler
 exports.handler = (event, context, callback) => {
+  // Amazon SES passes information to the Lambda function in JSON format
+  // This format is described in 
+  // http://docs.aws.amazon.com/ses/latest/DeveloperGuide/receiving-email-notifications-contents.html 
+  var sesNotification = event.Records[0].ses;
+  console.log("SES Notification:\n", JSON.stringify(sesNotification, null, 2));
+  callback(null, null);
 };
